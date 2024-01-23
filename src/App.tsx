3a8149be9a9ed2ef7fd5f282c8079522dbc81c53
PhamManhCuong <cuongpham.251203@gmail.com>
2024-01-24 01:39:06 +0700

    call Pokemon API

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+// lấy data từ https://pokeapi.co/
+
+interface Pokemons {
+  name: string,
+  url: string
+}
+
+interface Pokemon {
+  id: number,
+  name: string,
+  // thuộc tính này là để lấy ra avatar của pokemon
+  sprites: {
+    front_default: string
+  }
+}
 
 // để biết này là 1 react function component
 const App: React.FC = () => {
+
+  const [pokemons, setPokemons] = useState<Pokemon[]>([])
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20")
+      // vì trong res.data khi console.log có 1 property là result
+      res.data.results.forEach(async (pokemon: Pokemons) => {
+        const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        console.log(poke.data);
+        setPokemons((p) => [...p, poke.data])
+      })
+      // console.log(res.data);
+
+    }
+    getPokemon()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
